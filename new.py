eaae46fb55be20d5c52de51ed6c7eddd42519706
Andrew Aliaj <andyaliaj@gmail.com>
2024-05-02 16:30:11 -0400

    new

--- new.py
+++ new.py
@@ -1,1 +1,11 @@
 print(34)
+print(30)
+class Age:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def age(self):
+        c = int(self.b) - int(self.a)
+        return c
+    
+print(Age(1990, 2020).age())
